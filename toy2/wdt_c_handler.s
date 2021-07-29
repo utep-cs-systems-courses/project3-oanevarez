8a67b4b4f7c9f0467e238af6e38b48ad98ce6666
oanevarez <42950984+oanevarez@users.noreply.github.com>
2021-07-29 10:43:16 -0600

    Assembly code for wdt_c_handler in main

---
+++ toy2/wdt_c_handler.s
@@ -0,0 +1,55 @@
+	.data
+
+state:
+	.byte 1
+sec_count:
+	.word 0
+  
+  .text
+jt:
+	.word case_1
+  .word case_2
+  .word case_3
+  .word case_4
+  
+  .global wdt_c_handler
+  
+wdt_c_handler:
+	cmp #76, &secCount
+  jnz updateSec
+  mov #0, &secCount
+  cmp #4, &state
+  jc default
+  mov &state, r12
+  add r12, r12
+  mov jt(r12), r0
+  
+case_1:
+	call #move_up(3)
+	jmp end
+
+case_2:
+	call #move_down(3)
+	jmp end
+  
+case_3:
+	call #move_left(3)
+	jmp end
+  
+case_4:
+	call #move_right(3)
+	jmp end
+
+end:
+  pop r0 ; return 
+  
+  
+  .data
+updateSec:
+  add 31, &secCount
+  
+default:
+  mov #1, &state
+
+
+	
