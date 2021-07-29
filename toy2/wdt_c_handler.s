ba99f30719903de6bd6273f0306b3b52f50349d9
oanevarez <42950984+oanevarez@users.noreply.github.com>
2021-07-29 10:52:09 -0600

    Update wdt_c_handler.s

--- toy2/wdt_c_handler.s
+++ toy2/wdt_c_handler.s
@@ -5,24 +5,24 @@ state:
 sec_count:
 	.word 0
   
-  .text
+  	.text
 jt:
 	.word case_1
-  .word case_2
-  .word case_3
-  .word case_4
+  	.word case_2
+  	.word case_3
+  	.word case_4
   
-  .global wdt_c_handler
+  	.global wdt_c_handler
   
 wdt_c_handler:
 	cmp #76, &secCount
-  jnz updateSec
-  mov #0, &secCount
-  cmp #4, &state
-  jc default
-  mov &state, r12
-  add r12, r12
-  mov jt(r12), r0
+        jnz updateSec
+        mov #0, &secCount
+        cmp #4, &state
+  	jc default
+  	mov &state, r12
+  	add r12, r12
+  	mov jt(r12), r0
   
 case_1:
 	call #move_up(3)
@@ -41,15 +41,15 @@ case_4:
 	jmp end
 
 end:
-  pop r0 ; return 
+  	pop r0 ; return 
   
   
-  .data
+  	.data
 updateSec:
-  add 31, &secCount
+  	add 31, &secCount
   
 default:
-  mov #1, &state
+  	mov #1, &state
 
 
 	
